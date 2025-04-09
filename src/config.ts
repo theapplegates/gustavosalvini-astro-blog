fb679ec3d4c1615582bb0d3b8ffb532e1805137b
Gustavo Salvini <gsalvini@ecimtech.com>
2025-04-09 12:16:34 -0300

    more contributing config updates

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/guspatagonico/gustavosalvini-astro-blog/edit/main/",
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
